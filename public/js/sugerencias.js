1d172c3d7862ab45fe5f8664b31248e353983513
DavidTorr1 <129142867+DavidTorr1@users.noreply.github.com>
2025-04-28 14:31:00 -0600

    Add files via upload

---
+++ public/js/sugerencias.js
@@ -0,0 +1,22 @@
+// Código Fetch para traer la sugerencia de pan
+function obtenerSugerencia() {
+    fetch('https://sarimax-panaderia-v2-dyfwgmb5ecb5gnb4.eastus-01.azurewebsites.net')  // Recuerda: ahorita es LOCALHOST
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // Aquí ves qué te regresó Flask
+        const sugerencia = data.sugerencia;
+  
+        // Mostrar la sugerencia en el HTML
+        const contenedor = document.getElementById('sugerencia-pan');
+        contenedor.textContent = "Hoy te sugerimos: " + sugerencia;
+      })
+      .catch(error => {
+        console.error('Error al obtener sugerencia:', error);
+      });
+  }
+  
+  // Ejecutar la función cuando cargue la página
+  window.onload = function() {
+    obtenerSugerencia();
+  };
+  
